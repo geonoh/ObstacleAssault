f30e30d542c4050d7dfb5942f9098e8ee719f0ff
Geon Oh <ohhh123@naver.com>
2025-05-20 22:03:18 +0900

    Add variables in AMovingPlatform

--- Source/ObstacleAssault/MovingPlatform.h
+++ Source/ObstacleAssault/MovingPlatform.h
@@ -23,4 +23,12 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(EditAnywhere)
+	int32 MyInt = 99;
+
+	UPROPERTY(EditAnywhere)
+	float MyFloat = 5.99f;
+
+	UPROPERTY(EditAnywhere)
+	bool MyBool = true;
 };
